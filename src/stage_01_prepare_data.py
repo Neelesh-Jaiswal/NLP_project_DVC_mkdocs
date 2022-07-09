f8f02c65e82003050ff43a0ea60d2cb1de4e84a9
Neelesh-Jaiswal <nikhilneelesh1@gmail.com>
2022-07-10 00:31:02 +0530

    added tag value to params.yaml

--- src/stage_01_prepare_data.py
+++ src/stage_01_prepare_data.py
@@ -19,6 +19,7 @@ def main(config_path,params_path):
 
     split = params['prepare']['split']
     seed = params['prepare']['seed']
+    tag = params['prepare']['tag']
 
     random.seed(seed)
 
@@ -33,7 +34,7 @@ def main(config_path,params_path):
     with open(source_data_path,"r", encoding=encode) as fd_in: # actual input data that we are reading
         with open(train_data_path,"w", encoding=encode) as fd_out_train: # writing train data
             with open(test_data_path,"w", encoding=encode) as fd_out_test: # writing test data
-                process_posts(fd_in, fd_out_train, fd_out_test, "<python>", split)
+                process_posts(fd_in, fd_out_train, fd_out_test, tag, split)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
